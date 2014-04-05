25e2c91e90d5202f14cd618c03ac93abd7750f22
Filipp B <b_filip@zoho.com>
2014-04-05 21:09:09 +0400

    Include school_start in target fields

--- classification/vk.py
+++ classification/vk.py
@@ -170,7 +170,8 @@ def main():
         u'last_name': get_last_name,
         u'sex': get_sex,
         u'age': get_age,
-        u'graduation': get_graduation
+        u'graduation': get_graduation,
+        u'school_start': get_school_start
     }
     target_fields_list = [k for k in target_fields.iterkeys()]
     required_fields = get_required_fields(target_fields)
