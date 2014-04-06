29c7d758cfe952f5e37f50ec44dbb67e77a71a83
Filipp B <b_filip@zoho.com>
2014-04-06 20:54:41 +0400

    Add get_fields function

--- classification/tsv_import.py
+++ classification/tsv_import.py
@@ -1,1 +1,7 @@
 import io
+
+
+def get_fields(string, delimiter=u'\t'):
+    """Returns a list of fields in a line, separated by delimiter"""
+    fields = string.strip().split(delimiter)
+    return fields
