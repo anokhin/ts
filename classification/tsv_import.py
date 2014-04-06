70a5c340ae127691f1222f3d84ca66cffc14dfb5
Filipp B <b_filip@zoho.com>
2014-04-06 20:55:27 +0400

    Add function for parsing tsv file

--- classification/tsv_import.py
+++ classification/tsv_import.py
@@ -5,3 +5,12 @@ def get_fields(string, delimiter=u'\t'):
     """Returns a list of fields in a line, separated by delimiter"""
     fields = string.strip().split(delimiter)
     return fields
+
+
+def parse_tsv(stream, delimiter=u'\t'):
+    """Takes a stream
+    Returns a generator of lists,
+    those sublists are lists of fields for each line
+    """
+    for line in stream:
+        yield get_fields(line, delimiter)
