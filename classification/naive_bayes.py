d65075f7a0e319f5f28b68d6b748647b3df6cbfa
Filipp B <b_filip@zoho.com>
2014-04-10 04:11:37 +0400

    Add prototype and comment for NB add_classes

--- classification/naive_bayes.py
+++ classification/naive_bayes.py
@@ -4,3 +4,13 @@ class GaussianNB:
 
     def predict(sample):
         pass
+
+    def add_classes(classes):
+        """Manually configure, how big the classes for our regression are,
+        for example 0-10, 11-20, 21-30, etc.
+
+        Arguments:
+            classes - list of pairs, each pair corresponds to min and max
+            values of that class (for example (0,10) corresponds to 0-10
+        """
+        pass
