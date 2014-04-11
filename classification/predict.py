15d1a071709acc10a9df9a9b525ff9b277055689
Filipp B <b_filip@zoho.com>
2014-04-11 08:07:49 +0400

    predict.py main: refactor training data import

--- classification/predict.py
+++ classification/predict.py
@@ -28,51 +28,48 @@ if __name__ == '__main__':
         print "Please provide predict file"
         sys.exit(1)
 
-    with io.open(tsv_filename) as tsv_file:
-        with io.open(predict_filename) as predict_file:
-            CONVERSION_FUNCTIONS = {
-                u'age': str_to_int_or_none,
-                u'first_name': identity,
-                u'friends_age': str_to_int_or_none,
-                u'graduation': str_to_int_or_none,
-                u'last_name': identity,
-                u'school_end': str_to_int_or_none,
-                u'school_start': str_to_int_or_none,
-                u'uid': str_to_int_or_none
-            }
-            AGE_INTERVALS = [(0, 16), (17, 19),
-                             (20, 23), (24, 29), (30, 34), (35, 39),
-                             (40, 44), (45, 49), (50, 59), (60, 99)]
-            FEATURE_FIELDS = [
-                u'age', u'school_start', u'school_end', u'graduation',
-                u'friends_age']
-            parsed_tsv = list(tsv_import.parse_tsv(tsv_file))
-            dicts = tsv_import.lists_to_dicts(parsed_tsv, CONVERSION_FUNCTIONS)
-            user_lists = list(tsv_import.dicts_to_lists(
-                dicts,
-                FEATURE_FIELDS
-            ))
-            ages = [user[0] for user in user_lists]
-            age_intervals = tsv_import.break_on_intervals(ages, AGE_INTERVALS)
+    with io.open(predict_filename) as predict_file:
+        CONVERSION_FUNCTIONS = {
+            u'age': str_to_int_or_none,
+            u'first_name': identity,
+            u'friends_age': str_to_int_or_none,
+            u'graduation': str_to_int_or_none,
+            u'last_name': identity,
+            u'school_end': str_to_int_or_none,
+            u'school_start': str_to_int_or_none,
+            u'uid': str_to_int_or_none
+        }
+        AGE_INTERVALS = [(0, 16), (17, 19),
+                         (20, 23), (24, 29), (30, 34), (35, 39),
+                         (40, 44), (45, 49), (50, 59), (60, 99)]
+        FEATURE_FIELDS = [
+            u'age', u'school_start', u'school_end', u'graduation',
+            u'friends_age']
+        user_lists = tsv_import.get_data_from_file(tsv_filename,
+                                                   FEATURE_FIELDS,
+                                                   CONVERSION_FUNCTIONS)
+        ages = tsv_import.get_ages(user_lists, FEATURE_FIELDS)
 
-            classifier = naive_bayes.GaussianNB()
-            classifier.add_classes(AGE_INTERVALS)
-            classifier.fit(user_lists, age_intervals)
+        age_intervals = tsv_import.break_on_intervals(ages, AGE_INTERVALS)
 
-            parsed_tsv = list(tsv_import.parse_tsv(predict_file))
-            dicts = tsv_import.lists_to_dicts(parsed_tsv, CONVERSION_FUNCTIONS)
-            user_lists = list(tsv_import.dicts_to_lists(
-                dicts,
-                [u'age', u'school_start', u'school_end', u'graduation',
-                 u'friends_age']
-            ))
-            for u_dict, feature_list in zip(dicts, user_lists):
-                print u_dict
-                print classifier.predict(feature_list)
-                print
+        classifier = naive_bayes.GaussianNB()
+        classifier.add_classes(AGE_INTERVALS)
+        classifier.fit(user_lists, age_intervals)
 
-            print dir(classifier)
-            print classifier._GaussianNB__class_prior_probabilities
-            import pprint
-            pprint.pprint(classifier._GaussianNB__means)
-            pprint.pprint(classifier._GaussianNB__variances)
+        parsed_tsv = list(tsv_import.parse_tsv(predict_file))
+        dicts = tsv_import.lists_to_dicts(parsed_tsv, CONVERSION_FUNCTIONS)
+        user_lists = list(tsv_import.dicts_to_lists(
+            dicts,
+            [u'age', u'school_start', u'school_end', u'graduation',
+             u'friends_age']
+        ))
+        for u_dict, feature_list in zip(dicts, user_lists):
+            print u_dict
+            print classifier.predict(feature_list)
+            print
+
+        print dir(classifier)
+        print classifier._GaussianNB__class_prior_probabilities
+        import pprint
+        pprint.pprint(classifier._GaussianNB__means)
+        pprint.pprint(classifier._GaussianNB__variances)
