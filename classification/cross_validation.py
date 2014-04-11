dc32c035fcd74c867dfd8c1d0534c374a823cdf4
Filipp B <b_filip@zoho.com>
2014-04-11 23:49:39 +0400

    Implement regression mode - standard deviation

--- classification/cross_validation.py
+++ classification/cross_validation.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import random
+import numpy
 
 from predict import CONVERSION_FUNCTIONS, FEATURE_FIELDS, FRIENDS_INDEX
 from tsv_import import get_data_from_file, get_friends, determine_intervals
@@ -37,6 +38,72 @@ def cross_val_score(classifier_class, data, results, cv):
         )
         for i in range(cv)
     ]
+
+
+def choose_random_items_with_friends(data, results, amount, friends):
+    """Randomly chooses amount samples from data and results
+    Returns randomly chosen and unchosen data and results separately
+    """
+    chosen_indexes = random.sample(range(len(data)), amount)
+    chosen_data = []
+    chosen_results = []
+    chosen_friends = []
+    unchosen_data = []
+    unchosen_results = []
+    for i in range(len(data)):
+        if i in chosen_indexes:
+            chosen_data.append(data[i])
+            chosen_results.append(results[i])
+            chosen_friends.append(friends[i])
+        else:
+            unchosen_data.append(data[i])
+            unchosen_results.append(results[i])
+
+    return chosen_data, chosen_results, unchosen_data, unchosen_results, \
+        chosen_friends
+
+
+def cross_val_regression_score(classifier_class, data, results, cv, friends):
+    return [
+        regression_score(
+            classifier_class,
+            *choose_random_items_with_friends(
+                data, results, len(data) / cv, friends
+            ),
+            use_sklearn_nb=use_sklearn_nb
+        )
+        for i in range(cv)
+    ]
+
+
+def regression_score(
+    classifier_class,
+    data_to_predict, results_to_predict,
+    data_to_train, training_results,
+    friends_to_predict,
+    use_sklearn_nb
+):
+    classifier = classifier_class()
+    if use_sklearn_nb:
+        data_to_predict = numpy.array(data_to_predict)
+        prediction_correct_results = numpy.array(prediction_correct_results)
+        data_to_train = numpy.array(data_to_train)
+        training_results = numpy.array(training_results)
+
+    classifier.fit(data_to_train, training_results)
+    predicted_results = [
+        classifier.predict(sample) for sample in data_to_predict
+    ]
+    predicted_regression = [
+        numpy.mean(predicted_result) for predicted_result in predicted_results
+    ]
+    square_differences = [
+        (predicted - actual) ** 2
+        for predicted, actual in zip(predicted_regression, friends_to_predict)
+    ]
+    print predicted_regression
+    print
+    return numpy.sqrt(numpy.mean(square_differences))
 
 
 def main():
@@ -98,14 +165,9 @@ def main():
     for user in data:
         user.pop(FRIENDS_INDEX)
 
-    if not use_sklearn_nb:
-        scores = cross_val_score(
-            GaussianNB, data, age_classes, amount_of_groups
-        )
-    else:
-        scores = cross_val_score(
-            GaussianNB, data, numbered_classes, amount_of_groups
-        )
+    scores = cross_val_regression_score(
+        GaussianNB, data, age_classes, amount_of_groups, ages
+    )
     print scores
 
 
