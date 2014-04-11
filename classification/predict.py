fbc846f423c546d310a398c5c89f34da56aa6806
Filipp B <b_filip@zoho.com>
2014-04-11 08:15:27 +0400

    Refactor importing data for prediction

--- classification/predict.py
+++ classification/predict.py
@@ -1,6 +1,5 @@
 import argparse
 import sys
-import io
 
 import tsv_import
 import naive_bayes
@@ -28,48 +27,39 @@ if __name__ == '__main__':
         print "Please provide predict file"
         sys.exit(1)
 
-    with io.open(predict_filename) as predict_file:
-        CONVERSION_FUNCTIONS = {
-            u'age': str_to_int_or_none,
-            u'first_name': identity,
-            u'friends_age': str_to_int_or_none,
-            u'graduation': str_to_int_or_none,
-            u'last_name': identity,
-            u'school_end': str_to_int_or_none,
-            u'school_start': str_to_int_or_none,
-            u'uid': str_to_int_or_none
-        }
-        AGE_INTERVALS = [(0, 16), (17, 19),
-                         (20, 23), (24, 29), (30, 34), (35, 39),
-                         (40, 44), (45, 49), (50, 59), (60, 99)]
-        FEATURE_FIELDS = [
-            u'age', u'school_start', u'school_end', u'graduation',
-            u'friends_age']
-        user_lists = tsv_import.get_data_from_file(tsv_filename,
-                                                   FEATURE_FIELDS,
-                                                   CONVERSION_FUNCTIONS)
-        ages = tsv_import.get_ages(user_lists, FEATURE_FIELDS)
+    CONVERSION_FUNCTIONS = {
+        u'age': str_to_int_or_none,
+        u'first_name': identity,
+        u'friends_age': str_to_int_or_none,
+        u'graduation': str_to_int_or_none,
+        u'last_name': identity,
+        u'school_end': str_to_int_or_none,
+        u'school_start': str_to_int_or_none,
+        u'uid': str_to_int_or_none
+    }
+    AGE_INTERVALS = [(0, 16), (17, 19),
+                     (20, 23), (24, 29), (30, 34), (35, 39),
+                     (40, 44), (45, 49), (50, 59), (60, 99)]
+    FEATURE_FIELDS = [
+        u'age', u'school_start', u'school_end', u'graduation',
+        u'friends_age']
+    user_lists = tsv_import.get_data_from_file(tsv_filename,
+                                               FEATURE_FIELDS,
+                                               CONVERSION_FUNCTIONS)
+    ages = tsv_import.get_ages(user_lists, FEATURE_FIELDS)
 
-        age_intervals = tsv_import.break_on_intervals(ages, AGE_INTERVALS)
+    age_intervals = tsv_import.break_on_intervals(ages, AGE_INTERVALS)
 
-        classifier = naive_bayes.GaussianNB()
-        classifier.add_classes(AGE_INTERVALS)
-        classifier.fit(user_lists, age_intervals)
+    classifier = naive_bayes.GaussianNB()
+    classifier.add_classes(AGE_INTERVALS)
+    classifier.fit(user_lists, age_intervals)
 
-        parsed_tsv = list(tsv_import.parse_tsv(predict_file))
-        dicts = tsv_import.lists_to_dicts(parsed_tsv, CONVERSION_FUNCTIONS)
-        user_lists = list(tsv_import.dicts_to_lists(
-            dicts,
-            [u'age', u'school_start', u'school_end', u'graduation',
-             u'friends_age']
-        ))
-        for u_dict, feature_list in zip(dicts, user_lists):
-            print u_dict
-            print classifier.predict(feature_list)
-            print
+    features_lists_to_predict = tsv_import.get_data_from_file(
+        predict_filename, FEATURE_FIELDS, CONVERSION_FUNCTIONS)
 
-        print dir(classifier)
-        print classifier._GaussianNB__class_prior_probabilities
-        import pprint
-        pprint.pprint(classifier._GaussianNB__means)
-        pprint.pprint(classifier._GaussianNB__variances)
+    for user_features in features_lists_to_predict:
+        print user_features, classifier.predict(user_features)
+
+    import pprint
+    pprint.pprint(classifier._GaussianNB__means)
+    pprint.pprint(classifier._GaussianNB__variances)
