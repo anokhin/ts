8764d47ab20c2eb019cd1a78c17e12810067857e
Filipp B <b_filip@zoho.com>
2014-04-11 09:52:39 +0400

    naive_bayes: determine classes from results

--- classification/naive_bayes.py
+++ classification/naive_bayes.py
@@ -14,6 +14,7 @@ class GaussianNB:
             in the same order. Each value here must first be added with
             add_classes method.
         """
+        self.__classes = sorted(list(set(results)))
         self.__check_classes(results)
         assert len(samples) == len(results)
         self.__class_prior_probabilities = {
@@ -65,18 +66,6 @@ class GaussianNB:
             return 1
         return (e ** (-1) * ((float(value) - mean) ** 2) / (2 * variance)) / \
             sqrt(2.0 * pi * variance)
-
-    def add_classes(self, classes):
-        """Manually configure, how big the classes for our regression are,
-        for example 0-10, 11-20, 21-30, etc.
-
-        Arguments:
-            classes - list of pairs, each pair corresponds to min and max
-            values of that class (for example (0,10) corresponds to 0-10.
-            Must not overlap, each next pair's values must be bigger than
-            previous pair's values
-        """
-        self.__classes = classes
 
 
 def gaussian_variance(numbers):
