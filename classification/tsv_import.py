aea5e9b3e0e61134329386f677070ab0b5353251
Filipp B <b_filip@zoho.com>
2014-04-11 21:53:12 +0400

    tsv_import: properly handle quotes

--- classification/tsv_import.py
+++ classification/tsv_import.py
@@ -1,14 +1,25 @@
 import io
 from sys import argv
-import re
+import csv
 
 
-PATTERN = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
 
 
 def get_fields(string):
     """Returns a list of fields in a line, separated by delimiter"""
-    return PATTERN.split(string.strip())[1::2]
+    return unicode_csv_reader([string], delimiter=',').next()
 
 
 def parse_tsv(stream):
