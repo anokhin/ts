11ff8d3eb50f4e727dc853e7212c6c26fc3a4a6c
Nikolay Anokhin <n.anokhin@corp.mail.ru>
2014-06-01 20:46:22 +0400

    Hadoop tutorial 2

--- hadoop/user_activity.py
+++ hadoop/user_activity.py
@@ -8,18 +8,50 @@ __author__ = 'Nikolay Anokhin'
 class Mapper:
 
     def __init__(self):
-        with open("ids.txt", "rU") as ids_file:
-            self.ids = dict(line.strip().lower().split() for line in ids_file)
+        self.ids = {}
+        with open("tv_train.txt", "rU") as ids_file:
+            for line in ids_file:
+                items = line.strip().split("\t")
+                if len(items) == 2:
+                    self.ids[items[0]] = items[1]
+                if len(self.ids) > 100000:
+                    break
+
 
     def __call__(self, key, value):
         items = value.split("\t")
         user_id = items[0]
-        if user_id in self.ids:
-            yield user_id, 1
+        gender_str = items[2]
+        age_str = items[3]
+        # if user_id in self.ids:
+        if True:
+            if gender_str != "None":
+                gender = int(gender_str)
+                if gender:
+                    yield user_id, ("female", 1)
+                else:
+                    yield user_id, ("male", 1)
+            if age_str != "None":
+                age = int(age_str)
+                if 10 < age < 80:
+                    yield user_id, ("age", age)
 
 
 def reducer(key, values):
-    yield key, sum(values)
+    ages = []
+    male_count = 0
+    female_count = 0
+    for feature, value in values:
+        if feature == "female":
+            female_count += value
+        elif feature == "male":
+            male_count += value
+        elif feature == "age":
+            ages.append(value)
+    if ages:
+        yield key, ("age", float(sum(ages)) / len(ages))
+    yield key, ("female", female_count)
+    yield key, ("male", male_count)
 
 
 if __name__ == "__main__":
